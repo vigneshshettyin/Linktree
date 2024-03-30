5b1ffb87797aabdcbc7dabd14d7a0e16ea8c76c8
Vignesh Shetty <vigneshshetty.in@gmail.com>
2024-03-31 00:27:46 +0530

    Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import ThemeState from "./state/theme/themeState";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.render(
   <ThemeState>
+    <Analytics />
     <App />
   </ThemeState>,
   document.getElementById("root")
